e6265b34e76b84a0340f343d3ff5158b4b240e82
José Teles <josetelesmaciel@gmail.com>
2024-05-30 10:17:35 -0300

    docs: add tailwind to storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import Button from "@/components/button/Button";
 
 export default function Home() {
   return (
     <main>
+      <Button label="Primary Button" />
     </main>
   );
 }
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,4 +1,3 @@
-// components/Button.tsx
 import React from 'react';
 
 type ButtonProps = {

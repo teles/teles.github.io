18923f8825345948b9a2a737665665b763a13963
José Teles <josetelesmaciel@gmail.com>
2024-05-30 12:47:18 -0300

    feat: Add Box component and its story

---
+++ src/components/box/Box.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export interface BoxProps {
+  centered?: boolean;
+  children: React.ReactNode;
+}
+
+const Box: React.FC<BoxProps> = ({ centered, children }) => {
+  return (
+    <div
+      className={`border border-gray-300 p-4 rounded-md ${centered ? 'flex justify-center items-center' : ''}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Box;

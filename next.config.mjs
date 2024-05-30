346b7d0d87c4cf5d89caeffb57da96eff1467a18
José Teles <josetelesmaciel@gmail.com>
2024-05-30 16:10:54 -0300

    feat: Update homepage metadata and add Open Graph and Twitter card information

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: { unoptimized: true }
 }
 
 export default nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import PageRenderer from "@/components/pageRenderer/PageRenderer"
-import sections from "../../home.json"
+import configs from "../../home.json"
 
 export default function Home() {
+  const { sections } = configs
   return (
     <main className="min-h-screen container m-auto py-10 md:py-20">
       <PageRenderer sections={sections} />

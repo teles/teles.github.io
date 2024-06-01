2efc511f1281d8b00dedbaf75f0057aef98ddcd4
José Teles <josetelesmaciel@gmail.com>
2024-05-31 21:45:23 -0300

    feat: Update home page layout and component styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss"
 
 const config: Config = {
+  darkMode: 'selector',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,6 +9,7 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      container: {},
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":

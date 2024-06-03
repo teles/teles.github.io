2981e35a2dd6dfccc6e491afcab5b9ce88d18d8e
José Teles <josetelesmaciel@gmail.com>
2024-06-02 21:39:18 -0300

    refactor: Update page.tsx to use PageRenderer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,13 @@
 "use client"
 import React from "react"
-import PageRenderer, { Section } from "@/components/pageRenderer/PageRenderer"
-import configs from "../../home.json"
-import Header from "@/components/header/Header"
+import page from "../../home.json"
+import PageRenderer from "@/components/pageRenderer/PageRenderer"
+import { Section } from "@/components/sectionsRenderer/SectionsRenderer"
 
 export default function Home() {
-  const { sections, footer } = configs as { sections: Section[], footer: Section[] }
-  const [ isDarkMode, setDarkMode] = React.useState(false)
-
-  const toggleDarkMode = () => {
-    setDarkMode(!isDarkMode)
-  }
+  const { sections, footer } = page as { sections: Section[], footer: Section[] }
 
   return (
-    <div className={`bg-white dark:bg-slate-900 ${isDarkMode ? 'dark' : ''}`}>
-      <Header title="Teles" toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
-      <main className="min-h-screen container max-w-[1200px] m-auto py-10 md:py-20 bg-white dark:bg-slate-900">
-        <PageRenderer sections={sections} />
-      </main>
-      <footer className="container max-w-[1200px] m-auto pt-10 pb-5 border-t border-gray-200 dark:border-slate-600">
-        <PageRenderer sections={footer} />
-      </footer>
-    </div>
+    <PageRenderer sections={sections} footer={footer} />
   )
 }
---
+++ src/components/sectionsRenderer/SectionsRenderer.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import ComponentsMap from '@/lib/ComponentsMap'
+
+interface ComponentProps {
+  type: string;
+  props: Record<string, unknown>;
+}
+
+type ColumnWidth = '1/2' | '1/3' | '2/3' | '1/4' | '3/4' | '1/5' | '2/5' | '3/5' | '4/5' | '1/6' | '5/6' | 'full'
+
+interface Column {  
+  width?: ColumnWidth;
+  components: ComponentProps[];
+}
+type Sizes = 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+
+interface SectionContainer {
+  top?: Sizes;
+  right?: Sizes;
+  bottom?: Sizes;
+  left?: Sizes;
+}
+
+export interface Section {
+  container?: SectionContainer;
+  columns: Column[];
+}
+
+interface SectionsRendererProps {
+  sections: Section[];
+}
+
+const SectionsRenderer: React.FC<SectionsRendererProps> = ({ sections }) => {
+  const widthMap: Record<ColumnWidth, string> = {
+    '1/2': 'w-full md:w-1/2 lg:w-1/2 xl:w-1/2',
+    '1/3': 'w-full md:w-1/2 lg:w-1/3 xl:w-1/3',
+    '2/3': 'w-full md:w-2/3 lg:w-2/3 xl:w-2/3',
+    '1/4': 'w-full md:w-1/4 lg:w-1/4 xl:w-1/4',
+    '3/4': 'w-full md:w-3/4 lg:w-3/4 xl:w-3/4',
+    '1/5': 'w-full md:w-1/5 lg:w-1/5 xl:w-1/5',
+    '2/5': 'w-full md:w-2/5 lg:w-2/5 xl:w-2/5',
+    '3/5': 'w-full md:w-3/5 lg:w-3/5 xl:w-3/5',
+    '4/5': 'w-full md:w-4/5 lg:w-4/5 xl:w-4/5',
+    '1/6': 'w-full md:w-1/6 lg:w-1/6 xl:w-1/6',
+    '5/6': 'w-full md:w-5/6 lg:w-5/6 xl:w-5/6',
+    'full': 'w-full'
+  }
+  const sizesBottomMap: Record<Sizes, string> = {
+    'none': '',
+    'xs': 'mb-4 md:mb-8',
+    'sm': 'mb-8 md:mb-12',
+    'md': 'mb-12 md:mb-16',
+    'lg': 'mb-16 md:mb-20',
+    'xl': 'mb-20 md:mb-32'
+  }
+
+  const sizesTopMap: Record<Sizes, string> = {
+    'none': '',
+    'xs': 'mt-4 md:mt-8',
+    'sm': 'mt-8 md:mt-12',
+    'md': 'mt-12 md:mt-16',
+    'lg': 'mt-16 md:mt-20',
+    'xl': 'mt-18 md:mt-32'
+  }
+
+  return (
+    <div>
+      {sections.map((section, sectionIndex) => (
+        <section key={sectionIndex} className={"container mx-auto "+sizesBottomMap[section?.container?.bottom ?? 'none']+" px-6 flex flex-col md:flex-row flex-wrap border-box " + sizesTopMap[section?.container?.top ?? 'none']}>
+          {section.columns.map((column, columnIndex) => (            
+            <div key={columnIndex} className={widthMap[column.width ?? 'full'] + ' pb-4 pr-4 md:pb-6 md:pr-6 flex flex-col'}>
+              {column.components.map((component, componentIndex) => {
+                const Component = ComponentsMap[component.type]
+                return Component ? (
+                  <Component key={componentIndex} {...component.props} />
+                ) : null
+              })}
+            </div>
+          ))}
+        </section>
+      ))}
+    </div>
+  )
+}
+
+export default SectionsRenderer

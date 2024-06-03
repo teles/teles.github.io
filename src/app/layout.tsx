c99ee051d4766822bfd948d142b02d0d5dcddb34
José Teles <josetelesmaciel@gmail.com>
2024-06-03 14:55:09 -0300

    refactor: Remove import paths for home.json in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
-import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import configs from "../content/home.json"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const { meta } = configs
-export const metadata: Metadata = meta
 
 export default function RootLayout({ children }: Readonly<{children: React.ReactNode}>) {
   return (

75b5e28fd46abd9081e7a99f1c171031e4cc7a0d
José Teles <josetelesmaciel@gmail.com>
2024-06-05 21:14:49 -0300

    fix: add metadata to home and projects pages

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,12 @@
-"use client"
 import React from "react"
 import page from "../../content/projects.json"
 import PageRenderer from "@/components/pageRenderer/PageRenderer"
 import { Section } from "@/components/sectionsRenderer/SectionsRenderer"
 import { PageContainer } from "@/components/pageRenderer/PageRenderer"
+import { Metadata } from 'next'
+
+const { meta } = page as { meta: Metadata }
+export const metadata: Metadata = meta
 
 export default function Projects() {
   const { sections, footer, container } = page as { sections: Section[], footer: Section[], container: PageContainer }

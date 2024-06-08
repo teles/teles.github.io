e199f8d041dc3c162b07fb98f61f6fa3da7b3bc0
José Teles <josetelesmaciel@gmail.com>
2024-06-08 12:10:18 -0300

    feat: Closes #11 Add Metadata component for managing page metadata

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -4,6 +4,7 @@ import paths from '@/lib/paths'
 import { Section } from '@/components/sectionsRenderer/SectionsRenderer'
 import PageRenderer from '@/components/pageRenderer/PageRenderer'
 import Home from "@/content/home.json"
+import Metadata from '@/components/metadata/Metadata'
 
 export async function generateStaticParams(): Promise<{ slug: string }[]> {
   return paths.filter((path) => path.route === 'projects')?.map((path) => {
@@ -27,31 +28,31 @@ interface Project {
   }[]
 }
 
-const projectToSection = (project: Project, content: string) : Section => {
+const projectToSection = (project: Project, content: string): Section => {
   return {
-    "columns": [
+    columns: [
       {
-        "components": [
+        components: [
           {
-            "type": "Article",
-            "props": {
-              "title": project.title,
-              "tags": project.tags,
-              "description": project.description,
-              "image":{
-                "src": project.image,
-                "alt": project.title
+            type: "Article",
+            props: {
+              title: project.title,
+              tags: project.tags,
+              description: project.description,
+              image: {
+                src: project.image,
+                alt: project.title,
               },
-              "content": content,
-              "skills": project.skills,
-              "links": project.links
-            }
-          }
-        ]
-      }
-    ]
-  }
-}
+              content: content,
+              skills: project.skills,
+              links: project.links,
+            },
+          },
+        ],
+      },
+    ],
+  };
+};
 
 export default async function ProjectPage({ params }: { params: { slug: string } }) {
     const filePath = paths.find((path) => path.slugs === params.slug)?.path
@@ -63,11 +64,14 @@ export default async function ProjectPage({ params }: { params: { slug: string }
     const { content, frontmatter: project } = getContent<Project>(filePath)
 
     return (
-      <PageRenderer
-        breadcrumbs={[{ title: "👨‍💻 Projects", href: "/projects" }, { title: project.title, href: "/projects/" + params.slug}]}
-        sections={[projectToSection(project, content)]}
-        footer={footer}
-        container={{}}
-      />
+      <>
+        <Metadata title={project.title} meta={[{ name: 'description', content: project.description }]} />
+        <PageRenderer
+          breadcrumbs={[{ title: "👨‍💻 Projects", href: "/projects" }, { title: project.title, href: "/projects/" + params.slug}]}
+          sections={[projectToSection(project, content)]}
+          footer={footer}
+          container={{}}
+        />
+      </>
     )
 }

bb823a325cb55d199a457522ffbb6f358c36f6a4
José Teles <josetelesmaciel@gmail.com>
2024-06-09 20:40:13 -0300

    feat: #14 Update paths.ts to dynamically generate project entries

--- src/lib/paths.ts
+++ src/lib/paths.ts
@@ -1,51 +1,17 @@
-import { Path } from './paths.types'
+import fs from 'fs'
+import path from 'path'
 
-const paths: Path[] = [
-  {
+const contentDirectory = path.join(process.cwd(), 'src/content/projects')
+const filenames = fs.readdirSync(contentDirectory)
+
+const paths = filenames.map(filename => {
+  const slug = filename.replace(/\.mdx$/, '')
+
+  return {
     route: 'projects',
-    slugs: 'personal-website',
-    path: '/src/content/projects/personal-website.mdx'
-  },
-  {
-    route: 'projects',
-    slugs: 'array-mixer',
-    path: '/src/content/projects/array-mixer.mdx'
-  },
-  {
-    route: 'projects',
-    slugs: 'holysheets',
-    path: '/src/content/projects/holysheets.mdx'
-  },
-  {
-    route: 'projects',
-    slugs: 'readme-converter-action',
-    path: '/src/content/projects/readme-converter-action.mdx'
-  },
-  {
-    route: 'projects',
-    slugs: 'awesome-seo',
-    path: '/src/content/projects/awesome-seo.mdx'
-  },
-  {
-    route: 'projects',
-    slugs: 'taildown',
-    path: '/src/content/projects/taildown.mdx'
-  },
-  {
-    route: "projects",
-    slugs: "pure-css-parallel-park",
-    path: "/src/content/projects/parallel-park.mdx"
-  },
-  {
-    route: "projects",
-    slugs: "miniapps",
-    path: "/src/content/projects/miniapps.mdx"
-  },
-  {
-    route: "projects",
-    slugs: "infinite-chocolate-bar",
-    path: "/src/content/projects/infinite-chocolate-bar.mdx"
+    slugs: slug,
+    path: `/src/content/projects/${filename}`,
   }
-]
+})
 
 export default paths
